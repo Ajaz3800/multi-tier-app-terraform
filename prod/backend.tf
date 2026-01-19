8b55d7298a665156c8736d851167c108101e1d7c
Ajaz3800 <shaikhajaz38000@gmail.com>
2026-01-19 17:48:20 +0530

    backend issue fixed

--- prod/backend.tf
+++ prod/backend.tf
@@ -1,7 +1,8 @@
 terraform {
     backend "s3" {
-        bucket         = var.bucket_name
+        bucket         = "multi-tier-web-backend-bucket-123456"
         key            = "prod/terraform.tfstate"
+        region         = "us-east-1"
         encrypt        = true
         dynamodb_table = "terraform-locks"
     }
--- prod/ec2-rabbitmq.tf
+++ prod/ec2-rabbitmq.tf
@@ -1,19 +1,3 @@
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["137112412989"] # Amazon
-
-}
 resource "aws_instance" "rabbitmq_instance" {
   ami                    = data.aws_ami.amazon_linux_2.id
   instance_type          = var.instance_type
